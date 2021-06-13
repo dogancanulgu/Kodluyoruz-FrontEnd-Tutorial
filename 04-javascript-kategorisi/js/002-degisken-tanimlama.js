d73f1be9e5d9ad6d24fe6069764a48e54b3a3689
dogancanulgu <78671556+dogancanulgu@users.noreply.github.com>
2021-06-13 18:51:11 +0300

    Değişken Tanımlama JS çalışıldı

---
+++ 04-javascript-kategorisi/js/002-degisken-tanimlama.js
@@ -0,0 +1,50 @@
+//***********let ve const ile Degisken Tanimlama***********/
+
+// var ile degisken tanımlama
+// var serverName="kodluyoruz.org"
+
+// console.log(serverName)
+
+// let ile degiskeni bos tanimlamak
+let serverName;
+console.log(serverName)
+
+// let ile degiskene bilgi aktarma
+serverName="kodluyoruz.org"
+console.log(serverName)
+
+// let ile degiskene bilgi atayarak tanimlamak
+let password="1234"
+console.log(password)
+
+// degisken atamasi yapmadan once kullanmaya calısmak
+
+// hatali kullanım
+// console.log(fullName)
+// let fullName="Dogancan Ülgü"
+
+let fullName="Dogancan Ülgü"
+
+// let ile tanimlanan degiskenin icindeki bilgiyi degistirmek
+fullName="Lorem Ipsum Dolor"
+console.log(fullName)
+
+// birlestirme ve ekleme islemi
+fullName + "Yeni Eklenen Bilgi"
+console.log(fullName+ " Test bilgisi")
+console.log(fullName)
+
+fullName = fullName+ " Yeni Bilgi"
+fullName = "2. Bilgi: "+ fullName
+
+fullName = "Sifirlandi"
+fullName += " ve yeni bilgi eklendi"
+console.log(fullName)
+
+// const ile degiskeni bos tanimlamaya calismak
+const SERVER_PASSWORD="adsadqewfwed"
+console.log(SERVER_PASSWORD)
+
+// SERVER_PASSWORD="asdasd"
+
+// const ile degisken tanimlamak
